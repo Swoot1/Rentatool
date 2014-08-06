700472c5cdc79a480b1f07ea196c0afd5fd2b560
Andy <andy.emilsson@gmail.com>
2014-08-06 17:36:08 +0200

    Testing branching

--- Public/Scripts/Controllers/NavigationController.js
+++ Public/Scripts/Controllers/NavigationController.js
@@ -1,5 +1,8 @@
 /**
- * Created by Elin on 2014-07-09.
+ * Created by Elin on 2014-07-09
+ *
+ *
+ *
  */
 rentaTool.controller('NavigationController', ['$scope', '$location', function ($scope, $location) {
     $scope.navigateToLogIn = function () {
