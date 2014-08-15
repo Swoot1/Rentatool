32e88360db8e4be7d4a532652289468e659556ab
Andy <andy.emilsson@gmail.com>
2014-08-15 22:46:50 +0200

    adding gui for user groups

---
+++ Public/Scripts/Controllers/UserGroupController.js
@@ -0,0 +1,25 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Andy
+ * Date: 2014-08-15
+ * Time: 21:31
+ * To change this template use File | Settings | File Templates.
+ */
+
+rentaTool.controller('UserGroupController', ['$scope', '$resource', '$location', '$routeParams', 'UserGroup', function ($scope, $resource, $location, $routeParams, UserGroup) {
+
+    if ($routeParams.id) {
+        $scope.userGroup = UserGroup.get({id: $routeParams.id});
+    } else {
+        $scope.userGroup = new UserGroup({});
+    }
+
+    $scope.returnToUserGroupList = function() {
+        $location.path('/usergroups');
+    };
+
+    $scope.createUserGroup = function () {
+        $scope.userGroup.$save({});
+    };
+
+}]);
