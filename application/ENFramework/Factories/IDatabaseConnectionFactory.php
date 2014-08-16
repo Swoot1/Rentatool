2ead493088c8bc46f95d841e1bc7b011e1c10c1e
elinnilsson <elinhejnilsson@gmail.com>
2014-08-16 11:59:54 +0200

    Dependency inject databaseconnection.

---
+++ application/ENFramework/Factories/IDatabaseConnectionFactory.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elinnilsson
+ * Date: 15/08/14
+ * Time: 19:28
+ */
+
+namespace Rentatool\Application\ENFramework\Factories;
+
+
+interface IDatabaseConnectionFactory{
+   public function getDatabaseConnection();
+} 
