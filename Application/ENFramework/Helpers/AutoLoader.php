e6c18a89f99bc1b1865c9df1105e1efb807ef84e
elinnilsson <elinhejnilsson@gmail.com>
2014-08-31 19:49:36 +0200

    Changed autoloader to a PSR-0 standard autoloader so that the casing is right when the files are loaded. With the old autoloader the debugger would not work because of that.

--- Application/ENFramework/Helpers/AutoLoader.php
+++ Application/ENFramework/Helpers/AutoLoader.php
@@ -10,12 +10,23 @@
 namespace Rentatool\Application\ENFramework\Helpers;
 
 
-class AutoLoader
-{
+class AutoLoader{
 
-    public function setUpAutoLoader()
-    {
-        spl_autoload_extensions('.php');
-        spl_autoload_register('spl_autoload');
-    }
+   public function setUpAutoLoader(){
+
+      spl_autoload_register(function ($className){
+         $className = ltrim($className, '\\');
+         $fileName  = '';
+
+         if ($lastNsPos = strrpos($className, '\\')){
+            $namespace = substr($className, 0, $lastNsPos);
+            $className = substr($className, $lastNsPos + 1);
+            $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+         }
+
+         $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+         require $fileName;
+      });
+   }
 }
