152e13317b28ea42471e1fa664e13001fbc15c9b
elinnilsson <elinhejnilsson@gmail.com>
2014-09-24 17:44:42 +0200

    Added dependency that was missing.

--- Application/Controllers/PricePlanController.php
+++ Application/Controllers/PricePlanController.php
@@ -15,6 +15,7 @@ use Application\ENFramework\Helpers\SessionManager;
 use Application\ENFramework\Models\DatabaseConnection;
 use Application\ENFramework\Models\Request;
 use Application\Mappers\RentalObjectMapper;
+use Application\Services\FileService;
 use Application\Services\PricePlanService;
 use Application\Services\RentalObjectService;
 
@@ -27,9 +28,10 @@ class PricePlanController {
     * @var \Application\Services\PricePlanService
     */
    private $pricePlanService;
+   private $fileService;
    private $response;
 
-   public function __construct(Request $request, PricePlanService $pricePlanService,
+   public function __construct(Request $request, PricePlanService $pricePlanService, FileService $fileService,
                                ResponseFactory $responseFactory){
       $this->request             = $request;
       $this->pricePlanService = $pricePlanService;
@@ -39,7 +41,7 @@ class PricePlanController {
    public function create(array $data){
       $currentUser = SessionManager::getCurrentUser();
       $rentalObjectMapper = new RentalObjectMapper(new DatabaseConnection(new DatabaseConnectionFactory(), new MySQLValueFormatter()));
-      $rentalObjectService = new RentalObjectService($rentalObjectMapper, $this->pricePlanService);
+      $rentalObjectService = new RentalObjectService($rentalObjectMapper, $this->pricePlanService, $this->fileService);
       $pricePlan = $this->pricePlanService->create($data, $currentUser, $rentalObjectService);
       return $this->response
          ->setStatusCode(201)
@@ -50,7 +52,7 @@ class PricePlanController {
    public function delete($id){
       $currentUser = SessionManager::getCurrentUser();
       $rentalObjectMapper = new RentalObjectMapper(new DatabaseConnection(new DatabaseConnectionFactory(), new MySQLValueFormatter()));
-      $rentalObjectService = new RentalObjectService($rentalObjectMapper, $this->pricePlanService);
+      $rentalObjectService = new RentalObjectService($rentalObjectMapper, $this->pricePlanService, $this->fileService);
       $this->pricePlanService->delete($id, $currentUser, $rentalObjectService);
       return $this->response
          ->setStatusCode(204);
