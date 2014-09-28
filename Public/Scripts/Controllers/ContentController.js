deac0d0c70d2e3652ee23331645b3d53e7887350
elinnilsson <elinhejnilsson@gmail.com>
2014-09-28 20:20:20 +0200

    Updated the registration page.

--- Public/Scripts/Controllers/ContentController.js
+++ Public/Scripts/Controllers/ContentController.js
@@ -1,3 +1,13 @@
 /**
  * Created by elinnilsson on 28/09/14.
  */
+(function () {
+   angular.module('Rentatool')
+      .controller('ContentController', ['$scope', '$location', function ($scope, $location) {
+         $scope.content = {};
+
+         $scope.$on('$locationChangeStart', function(){
+            $scope.content.setContainerClass = $location.path() !== '/authorization/login';
+         })
+      }]);
+})();
--- Public/Scripts/Controllers/RentalObjectController.js
+++ Public/Scripts/Controllers/RentalObjectController.js
@@ -2,27 +2,31 @@
  * Created by Elin on 2014-04-18.
  */
 
-rentaTool.controller('RentalObjectController', ['$scope', '$routeParams', 'RentalObject', '$location', 'RentalObjectService', function ($scope, $routeParams, RentalObject, $location, RentalObjectService) {
+(function () {
+   angular.module('Rentatool').controller('RentalObjectController', ['$scope', '$routeParams', 'RentalObject', '$location', 'RentalObjectService', function ($scope, $routeParams, RentalObject, $location, RentalObjectService) {
 
-   if ($routeParams.id) {
-      $scope.rentalObject = RentalObject.get({id: $routeParams.id});
-   } else {
-      $scope.rentalObject = new RentalObject({});
-   }
+      if ($routeParams.id) {
+         $scope.rentalObject = RentalObject.get({id: $routeParams.id});
+      } else {
+         $scope.rentalObject = new RentalObject({fileCollection: []});
+      }
 
-   $scope.createRentalObject = function () {
-      $scope.rentalObject.$save({});
-   };
+      $scope.createRentalObject = function () {
+         $scope.rentalObject.$save({});
+      };
 
-   $scope.updateRentalObject = function () {
-      $scope.rentalObject.$update({});
-   };
+      $scope.updateRentalObject = function () {
+         $scope.rentalObject.$update({});
+      };
 
-   $scope.returnToRentalObjectList = function () {
-      $location.path('/rentalobjects');
-   };
+      $scope.returnToRentalObjectList = function () {
+         $location.path('/rentalobjects');
+      };
 
-   $scope.$watch(RentalObjectService.getPhoto, function(photo){
-      $scope.rentalObject.fileCollection = [photo];
-   });
-}]);
+      $scope.$watch(RentalObjectService.getPhoto, function (photo) {
+         if (photo && photo.id) {
+            $scope.rentalObject.fileCollection = [photo];
+         }
+      });
+   }]);
+})();
