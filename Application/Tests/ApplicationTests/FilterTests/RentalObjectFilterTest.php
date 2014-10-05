93d2bd8ca13caeddf161f700546a09175b3ae12f
elinnilsson <elinhejnilsson@gmail.com>
2014-10-05 12:33:03 +0200

    Filtertests.

---
+++ Application/Tests/ApplicationTests/FilterTests/RentalObjectFilterTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elinnilsson
+ * Date: 05/10/14
+ * Time: 11:10
+ */
+
+namespace Tests\FilterTests;
+
+
+class RentalObjectFilterTest {
+
+} 
---
+++ Application/Tests/ApplicationTests/FilterTests/UnavailableRentPeriodFilterTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elinnilsson
+ * Date: 05/10/14
+ * Time: 11:36
+ */
+
+namespace Tests\FilterTests;
+
+
+class UnavailableRentPeriodFilter {
+
+} 
