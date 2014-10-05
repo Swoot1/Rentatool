51f90e30698955fc4e462337ec3f48a4b63c69e2
Andy <andy.emilsson@gmail.com>
2014-10-05 19:04:37 +0200

    Test cases for ResponseData

--- Application/Tests/ENFrameworkTests/ResponseTests/ResponseTest.php
+++ Application/Tests/ENFrameworkTests/ResponseTests/ResponseTest.php
@@ -7,7 +7,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-namespace Rentatool\Tests\PHPFrameworkTests\HelperTests;
+namespace Rentatool\Tests\PHPFrameworkTests\ResponseTests;
 
 
 use Application\Models\MenuItem;
