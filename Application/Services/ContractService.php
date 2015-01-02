ac27c47618c8e8f7b51f25a9edeb552a025a18ca
Andy <andy.emilsson@gmail.com>
2015-01-02 22:07:45 +0100

    base for contract integration

---
+++ Application/Services/ContractService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Andy
+ * Date: 2015-01-02
+ * Time: 21:31
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Application\Services;
+
+
+class ContractService {
+
+}
