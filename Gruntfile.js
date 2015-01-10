16d95972530dc31a3e6aa3e729f2052ec03f6403
elinnilsson <elinhejnilsson@gmail.com>
2015-01-10 15:15:46 +0100

    Added fontendvalidation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,10 +47,11 @@ module.exports = function (grunt) {
                   'dest/default_options.js': [
                      'Public/css/Foundation/js/vendor/modernizr.js',
                      'Public/css/Foundation/js/vendor/fastclick.js',
-                     'Public/css/Foundation/js/vendor/jquery.js',
+                     'Public/Scripts/Vendor/JQuery/jquery-1.11.2.js',
                      'Public/Scripts/Vendor/JQuery/jquery-ui.js',
                      'Public/Scripts/Vendor/JQuery/jquery.fileupload.js',
                      'Public/Scripts/Vendor/Angular/angular.js',
+                     'Public/Scripts/Vendor/Angular/angular-messages.js',
                      'Public/Scripts/Vendor/Angular/Plugins/angular-resource.js',
                      'Public/Scripts/Vendor/Angular/Plugins/angular-route.js',
                      'Public/Scripts/Vendor/JQuery/jquery.iframe-transport.js',
@@ -59,6 +60,8 @@ module.exports = function (grunt) {
                      'Public/Scripts/app.js',
                      'Public/Scripts/Factories/*.js',
                      'Public/Scripts/Directives/*.js',
+                     'Public/Scripts/Directives/FormValidation/*.js',
+                     'Public/Scripts/Filters/*.js',
                      'Public/Scripts/Services/*.js',
                      'Public/Scripts/Controllers/*.js'
                   ]
@@ -69,7 +72,7 @@ module.exports = function (grunt) {
             options: {
                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */ \n',
                sourceMap: true,
-               sourceMapIncludeSources : true
+               sourceMapIncludeSources: true
 //               sourceMapIn: 'dist/<%= pkg.name %>.min.js.map' // TODO It should not be needed to include both full .js and min.js in html to get proper source map. That's the case right now. Future Elin can fix it.
             },
             dist: {
