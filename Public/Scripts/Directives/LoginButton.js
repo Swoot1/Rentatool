d4dbf6c5cb8d43ab12d700801fc6585ab8dac93f
elinnilsson <elinhejnilsson@gmail.com>
2015-01-16 16:02:06 +0100

    Fixed string true to regular true.

--- Public/Scripts/Directives/LoginButton.js
+++ Public/Scripts/Directives/LoginButton.js
@@ -6,8 +6,8 @@
         .directive('loginbutton', [function () {
             return {
                 restrict: 'A',
-                replace: 'true',
-                template: '<li ng-click="navigateToLogIn()" ng-hide="userIsLoggedIn" class="active"><a href="">Logga in</a></li>'
+               replace: true,
+               template: '<li ng-click="navigateToLogIn()" ng-hide="userIsLoggedIn" class="active"><a href="">Logga in</a></li>'
             };
         }]);
 })();
--- Public/Scripts/Directives/LogoutButton.js
+++ Public/Scripts/Directives/LogoutButton.js
@@ -6,8 +6,8 @@
         .directive('logoutbutton', [function () {
             return {
                 restrict: 'A',
-                replace: 'true',
-                template: '<li ng-click="attemptLogOut()" ng-show="userIsLoggedIn" class="active"><a href="">Logga ut</a></li>'
+               replace: true,
+               template: '<li ng-click="attemptLogOut()" ng-show="userIsLoggedIn" class="active"><a href="">Logga ut</a></li>'
             };
         }]);
 })();
