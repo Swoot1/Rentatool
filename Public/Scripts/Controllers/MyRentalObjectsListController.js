512ba7241b2fd14a98bce5eeb0b20f1abfa10f40
Andy <andy.emilsson@gmail.com>
2015-01-18 21:15:46 +0100

    Moved inactivation button to company overview page

--- Public/Scripts/Controllers/MyRentalObjectsListController.js
+++ Public/Scripts/Controllers/MyRentalObjectsListController.js
@@ -6,14 +6,23 @@
  * To change this template use File | Settings | File Templates.
  */
 (function () {
-    angular.module('Rentatool').controller('MyRentalObjectsListController', ['$scope', 'NavigationService', 'RentalObject', 'User',  function ($scope, NavigationService, RentalObject, User) {
-
-        // TODO: User rentalobjects resource and add status or create new resource?
+    angular.module('Rentatool').controller('MyRentalObjectsListController', ['$scope', 'NavigationService', 'RentalObject', 'User', 'InactivateRentalObject',  function ($scope, NavigationService, RentalObject, User, InactivateRentalObject) {
 
         User.get({'id': 'currentUser'}, function(data) {
             $scope.rentalObjectCollection = RentalObject.query({userId: data.id});
             $scope.currentUser = data.id;
         });
+
+        $scope.inactivateRentalObject = function (rentalObject) {
+            var indexOfRentalObject;
+            var inactivateRentalObjectResource = new InactivateRentalObject(rentalObject);
+
+            inactivateRentalObjectResource.$update({id: rentalObject.id},
+                function () {
+                    indexOfRentalObject = $scope.rentalObjectCollection.indexOf(rentalObject);
+                    $scope.rentalObjectCollection.splice(indexOfRentalObject, 1);
+                });
+        };
 
         $scope.navigateToRentalObject = NavigationService.navigateToRentalObject;
 
--- Public/Scripts/Controllers/RentalObjectListController.js
+++ Public/Scripts/Controllers/RentalObjectListController.js
@@ -2,7 +2,7 @@
  * Created by Elin on 2014-06-12.
  */
 (function () {
-   angular.module('Rentatool').controller('RentalObjectListController', ['$scope', '$location', 'RentalObject', 'InactivateRentalObject', 'User', 'NavigationService', 'UserService', function ($scope, $location, RentalObject, InactivateRentalObject, User, NavigationService, UserService) {
+   angular.module('Rentatool').controller('RentalObjectListController', ['$scope', '$location', 'RentalObject', 'User', 'NavigationService', 'UserService', function ($scope, $location, RentalObject, User, NavigationService, UserService) {
       $scope.rentalObjectCollection = RentalObject.query();
       $scope.rentalObjectFilter = {};
 
@@ -21,17 +21,6 @@
 
          $scope.rentalObjectCollection = RentalObject.query(
             GETParams);
-      };
-
-      $scope.inactivateRentalObject = function (rentalObject) {
-         var indexOfRentalObject;
-         var inactivateRentalObjectResource = new InactivateRentalObject(rentalObject);
-
-         inactivateRentalObjectResource.$update({id: rentalObject.id},
-            function () {
-               indexOfRentalObject = $scope.rentalObjectCollection.indexOf(rentalObject);
-               $scope.rentalObjectCollection.splice(indexOfRentalObject, 1);
-            });
       };
 
       $scope.getThumbNailURL = function (rentalObject) {
