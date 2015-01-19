3ae6be5c4340936cefe9f5aff17d81599cc2a219
elinnilsson <elinhejnilsson@gmail.com>
2015-01-19 22:18:37 +0100

    Moved pagination to separate file.

--- Public/Scripts/Controllers/RentalObjectListController.js
+++ Public/Scripts/Controllers/RentalObjectListController.js
@@ -2,69 +2,20 @@
  * Created by Elin on 2014-06-12.
  */
 (function () {
-   angular.module('Rentatool').controller('RentalObjectListController', ['$scope', '$location', 'RentalObject', 'User', 'NavigationService', 'UserService', function ($scope, $location, RentalObject, User, NavigationService, UserService) {
-
-      $scope.pagination = {
-         page: 1,
-         entryLimit: 3,
-         numberOfPages: 1,
-         goToFirstPage: function () {
-            this.page = 1;
-         },
-         goToPreviousPage: function () {
-            this.page--;
-         },
-         goToNextPage: function () {
-            this.page++;
-         },
-         goToLastPage: function () {
-            this.page = this.numberOfPages;
-         },
-         pages: [1]
-      };
-
-      $scope.$watchGroup(['pagination.page', 'pagination.numberOfPages'], function () {
-         var pageNumber = $scope.pagination.page, k = 0;
-         var diff = $scope.pagination.numberOfPages - $scope.pagination.page;
-         var i = diff === 0 ? -2 : (diff === 1 ? -1 : 0);
-
-         $scope.pagination.pages = [];
-
-         while (pageNumber > 1 && i < 2) {
-            pageNumber--;
-            i++;
-         }
-
-         while (k < 5 && pageNumber <= $scope.pagination.numberOfPages) {
-            $scope.pagination.pages.push(pageNumber);
-            pageNumber++;
-            k++;
-         }
-      });
-
-      $scope.$watch('totalNumberOfRows', function () {
-         $scope.pagination.numberOfPages = Math.ceil($scope.totalNumberOfRows / $scope.pagination.entryLimit);
-      });
-
-      $scope.$watch('pagination.page', function (newValue) {
-         $scope.setPage(newValue);
-      });
-
-      $scope.setPage = function (page) {
-         $scope.pagination.page = page;
-         $scope.getRentalObjects();
-      };
+   angular.module('Rentatool').controller('RentalObjectListController', ['$scope', '$location', 'RentalObject', 'User', 'NavigationService', 'UserService', 'PaginationService', function ($scope, $location, RentalObject, User, NavigationService, UserService, PaginationService) {
+      var GETParameters;
 
       $scope.getRentalObjects = function () {
-         var GETParameters = $scope.rentalObjectFilter;
+         GETParameters = $scope.rentalObjectFilter;
          GETParameters.page = $scope.pagination.page;
          GETParameters.entryLimit = $scope.pagination.entryLimit;
          $scope.rentalObjectCollection = RentalObject.query(GETParameters);
       };
 
+      PaginationService.setPagination($scope, $scope.getRentalObjects);
+
       $scope.searchRentalObject = function () {
-         $scope.pagination.page = 1;
-         $scope.getRentalObjects();
+         PaginationService.setPage(1);
       };
 
       $scope.rentalObjectFilter = {};
